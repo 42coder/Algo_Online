3b1aa733717a9fbbccd6a59b737bf2453ccf5e18
42coder <shreyas.singh@iitgn.ac.in>
2017-07-10 17:57:52 +0530

    fixed inversions.cpp started storing used test cases in test_cases.txt

--- algo_stan/inversion.cpp
+++ algo_stan/inversion.cpp
@@ -1,147 +1,90 @@
-//ERROR WHY YOU COMING!!!
-
 #include <iostream>
-//#include <fstream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-//int sortCount(int A[], int temp[], int l, int r);                     	//r is the rightmost index
-//int mergeCount(int A[],int temp[], int l, int m,int r);
+int sortCount(int arr[], int l, int r);
+int countSplit(int arr[], int l, int m, int r);
 
-/*int sortCountStart(int A[], int array_size)				//To make array temp
-    int *temp = new int[array_size];
-    return sortCount(A, temp, 0, array_size-1);
+void print_arr(int arr[], int size){
+	for (int i = 0; i < size; i++){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void accArr(){
+	ifstream fin;
+	fin.open("test.txt");
+
+	int val, size;
+
+	while (fin >> val){				//new way of accepting arrays
+		size = val;
+		int *arr;
+		arr = new int[size];
+
+		for (int i = 0; i < size; i++){
+			fin >> val;
+			arr[i] = val;
+		}
+
+		cout << sortCount(arr, 0, size-1) << "\t";
+		print_arr(arr, size);
+
+		delete[] arr;
+	}
 }
 
 int main(){
-    int arr[5] = {1, 20, 6, 4, 5};
-    cout << sortCountStart(arr,5) << endl;
-    for (int i=0; i<5; i++)
-	cout << arr[i] << " ";
-    cout << endl;
+	accArr();
 }
 
-int sortCount(int A[], int temp[], int l, int r){
-    if (l == r){
-        return 0;
-    }
-    else{
-	int inv_count = 0;					//Don't forget about +=
-	cout << inv_count;
-	inv_count  = sortCount(A, temp, l, (l+r)/2);
-	inv_count += sortCount(A, temp, (l+r)/2 + 1, r);		//Don't do r/2 like last time
-	inv_count += mergeCount(A, temp, l, (l+r)/2, r);
-	return inv_count;
-    }
-}
 
-int mergeCount(int A[], int temp[], int l, int m, int r){	//To count split inversions
-    int  i,j,k;
-    int inv_count = 0;
-    i = k = l;	//i - A and k - temp
-    j = m;
-    while (i < m && j <= r){
-	if (A[i] <= A[j]){
-	    temp[k++] = A[i++];
+int sortCount(int arr[], int l, int r){	//r is rightmost index
+	if (l < r){
+		int m = l+(r-l)/2;	//Avoids overflow for large l and r
+		int sum = 0;
+
+		sum = sortCount(arr,l,m);
+		sum += sortCount(arr,m+1,r);	//m+1 is important
+		sum += countSplit(arr,l,m,r);
+
+		return sum;
 	}
 	else{
-	    temp[k++] = A[j++];
-	    cout << "(" << A[i] << "," << A[j] << ")\n";
-	    inv_count++;		//If the number after m is bigger it's an inversion
+		return 0;
 	}
-    }
-    while (i < m){
-	temp[k++] = A[i++];
-	inv_count++;			//All numbers left before m are added at the end and so are inversions
-    }
-    while (j <= r){
-	temp[k++] = A[j++];
-    }
-    for (i=l; i <= r; i++)	//Copy back
-        A[i] = temp[i];
-    return inv_count;
-}*/
-
-int mergeSort (int arr[], int l, int r);
-void printArr (int arr[], int r);
-int Merge (int arr[], int l, int m, int r);
-
-int arr[]={1, 20, 6, 4, 5};
-const int arr_size = sizeof(arr)/sizeof(arr[0]);
-
-int main(){
-    printArr(arr,arr_size);
-
-    cout << "\n" << mergeSort(arr,0,arr_size-1) << "\n";
-
-    printArr(arr,arr_size);
-
-    cout << endl;
-
-    return 0;
 }
 
-int mergeSort (int arr[], int l, int r){
-    if (l < r){
-        // Same as (l+r)/2, but avoids overflow for large array sizes
-        int m = (r+l)/2;
-     	int inv_count;
+int countSplit(int arr[], int l, int m, int r){
+	int n1 = m-l+1, n2 = r-m;	//Needed for code readability
+	int L[n1], R[n2];		//temp left and right subparts of the array
+	int i, j, k;			//pointer to the array
+	int inv = 0;
 
-	inv_count = mergeSort(arr, l, m);
-	inv_count += mergeSort(arr, m+1, r);
-	inv_count += Merge(arr, l, m, r);
+	for (i = 0; i <= n1; i++)
+		L[i] = arr[l+i];
+	for (j = 0; j <= n2; j++)
+		R[j] = arr[m+1+j];
 
-        printArr(arr,arr_size);
-	cout << "\t" << inv_count << endl;
-	return inv_count;
-    }
-}
+	i = j = 0;
+	k = l;
 
-int Merge(int arr[],int l, int m, int r){
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 =  r - m;
-    int inv_count=0;
-
-    int M[n1], N[n2];
-    for (i = 0; i < n1; i++)
-        M[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        N[j] = arr[m + 1+ j];
-
-    i = 0; j = 0; k = l;
-    while (i<n1 && j<n2){
-        if (M[i] <= N[j]){
-            arr[k] = M[i];
-            i++;
-            //cout << "yo1";
-        }
-        else{
-            arr[k] = N[j];
-	    cout << "\n(" << M[i] << "," << N[j] << ")";
-            j++;
-	    inv_count++;
-            //cout << "lll";
-        }
-        k++;
-    }
-    cout << endl;
-    while (i<n1){
-        arr[k] = M[i];
-	cout << M[i] << " ";
-        i++; k++;
-	inv_count++;
-    }
-    while (j<n1){
-        arr[k] = N[j];
-        j++; k++;
-    }
-    return inv_count;
-}
-
-void printArr(int arr[], int r){
-    for (int i=0; i<r; i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+	while (i < n1 && j < n2){
+		if (L[i] > R[j]){
+			arr[k++] = R[j++];
+			inv += n1-i;			//If an element in the second half is bigger
+		}					//then no of inv = no of elements left in
+		else					//second array
+			arr[k++] = L[i++];
+	}
+	while (i < n1){
+		arr[k++] = L[i++];
+	}
+	while (j < n2){
+		arr[k++] = R[j++];
+	}
+	return inv;
 }
