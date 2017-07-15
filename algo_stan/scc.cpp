ffb709e50b534b1a8be9bcd9a4b179998e6b0467
42coder <shreyas.singh@iitgn.ac.in>
2017-07-15 12:01:15 +0530

    Udated to become a broken code which looks really ugly

--- algo_stan/scc.cpp
+++ algo_stan/scc.cpp
@@ -9,59 +9,105 @@
 using namespace std;
 
 class scc : public Graph {
-    bool *visited;
+    int t; 				//finsihing times for 1st pass
+    int S;				//current source vertex
+    bool *visited;			//for dfs
+    int *finTime;			//index is the finish time and the val is the node
+    int *leader;
+    vector<pair<int, int> > *trp;
 public:
     scc(int V):Graph(V){
+	trp = new vector<pair<int, int> >[V];
         visited = new bool(V);
+	finTime = new int(V);
+	leader = new int(V);
+	S = -1;
+	t = 0;
     };					//As we have inhereted the class Graph
     void dfsUtil(int v);
-    void dfs();
+    void dfsSec(int v);
+    void printScc();
+    void addTrp(int u, int v, int wt = 1);
 };
 
-void scc::dfs(){
+void scc::addTrp(int u, int v, int wt){
+    trp[v].push_back(make_pair(u, wt));
+}
+
+void scc::printScc(){
     for (int i=0; i<V; i++){
 	visited[i] = false;
     }
-    for (int i=0;i<V; i++){
+    for (int i = V-1; i >= 0; i++){				//Always go down
     	if (visited[i] == false){
 	    dfsUtil(i);
 	}
     }
+    for (int i=0; i<V; i++){
+	visited[i] = false;
+    }
+    for (int i = V-1; i >= 0; i++){
+	int j = finTime[i];
+	if (visited[j] == false){
+	    S = j;
+	    dfsSec(j);
+	}
+    }
+    for (int i = 0; i < V; i++)
+	cout << i << " " << finTime[i] << " " << leader[i] << endl;
 }
 
-void scc::dfsUtil(int v){
-    visited[v] = true;
-    Stack stack;
+void scc::dfsUtil(int v){					//Doing using stack as the file in the
+    visited[v] = true;						//coursera course is huge and may exceed
+    Stack stack;						//recursion limit
     stack.push(v);
     bool flag = false;
-    cout << v << " ";
+    //cout << v << " ";						//Better to write 2 code or a code to reverse graph?
     do{
 	vector< pair<int,int> >::iterator i;
-	for (i = adj[v].begin(); i < adj[v].end(); i++){
-	    if (visited[i->first] == false){
-                visited[i->first] = true;
-                stack.push(i->first);
+	for (i = trp[v].begin(); i < trp[v].end(); i++){
+    	    if (visited[i->first] == false){
+               	visited[i->first] = true;
+               	stack.push(i->first);
 		cout << i->first << " ";
 		flag = true;
 		break;
 	    }
 	}
 	if (flag == false){
-	    stack.pop();
+	    int j = stack.pop();
+	    t++;
+	    finTime[j] = t;
 	}
         v = stack.status();
 	flag = false;
     }while(v != -1);
+}
 
-/*// Mark the current node as visited and print it - gfg
-    visited[v] = true;
-    cout << v << " ";
-
-    // Recur for all the vertices adjacent to this vertex
-    list<int>::iterator i;
-    for(i = adj[v].begin(); i != adj[v].end(); ++i)
-        if(!visited[*i])
-            dfsUtil(*i, visited);*/
+void scc::dfsSec(int v){					//Doing using stack as the file in the
+    visited[v] = true;						//coursera course is huge and may exceed
+    Stack stack;						//recursion limit
+    stack.push(v);
+    bool flag = false;
+    //cout << v << " ";						//Better to write 2 code or a code to reverse graph?
+    do{
+	vector< pair<int,int> >::iterator i;
+	for (i = adj[v].begin(); i < adj[v].end(); i++){
+    	    if (visited[i->first] == false){
+               	visited[i->first] = true;
+               	stack.push(i->first);
+		cout << i->first << " ";
+		flag = true;
+		break;
+	    }
+	}
+	if (flag == false){
+	    int j = stack.pop();
+	    leader[j] = S;
+	}
+        v = stack.status();
+	flag = false;
+    }while(v != -1);
 }
 
 
@@ -75,10 +121,10 @@ int main(){
     while (fin >> a){		//Build graph
 	fin >> b;
 	g.addEdge(a,b);
+        g.addTrp(a,b);
     }
 
-    cout << "Following is Depth First Traversal\n";
-    g.dfs();
+    g.printScc();
     cout << endl;
     return 0;
 }
